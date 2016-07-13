03f66aff9d81576b961747d15fd4fe5af207c2de
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-07-14 07:14:52 +0900

    Add export mrb_state to Julia

--- mrblib/mrb_julia.rb
+++ mrblib/mrb_julia.rb
@@ -1,5 +1,18 @@
 class Julia
-  def bye
-    self.hello + " bye"
-  end
+    def self.export_mrb
+        jl = <<'EOS'
+function set_mrb_state(ptr::Ptr{Void})
+  global MRB_MRB_STATE = ptr
+end
+EOS
+        Julia.eval(jl)
+
+        jl = <<'EOS'
+function mrb_test()
+  r = ccall( (:mrb_load_string, "libmruby"), Void, (Ptr{Void},Cstring), MRB_MRB_STATE, "puts 'Hello,World!'")
+end
+EOS
+        Julia.eval(jl)
+        Julia.export_mrb_state
+    end
 end
--- src/mrb_julia.c
+++ src/mrb_julia.c
@@ -5,12 +5,11 @@
 **
 ** See Copyright Notice in LICENSE
 */
+#include <julia.h>
 
 #include "mruby.h"
 #include "mruby/data.h"
 #include "mrb_julia.h"
-
-#include <julia.h>
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
@@ -23,7 +22,8 @@ static const struct mrb_data_type mrb_julia_data_type = {
   "mrb_julia_data", mrb_free,
 };
 
-static mrb_value mrb_julia_init(mrb_state *mrb, mrb_value self)
+static mrb_value
+mrb_julia_init(mrb_state *mrb, mrb_value self)
 {
   mrb_julia_data *data;
 
@@ -34,59 +34,84 @@ static mrb_value mrb_julia_init(mrb_state *mrb, mrb_value self)
   DATA_TYPE(self) = &mrb_julia_data_type;
   DATA_PTR(self) = NULL;
 
-  //mrb_get_args(mrb, "s", &str, &len);
+  // mrb_get_args(mrb, "s", &str, &len);
   data = (mrb_julia_data *)mrb_malloc(mrb, sizeof(mrb_julia_data));
-  //data->str = str;
+  // data->str = str;
   data->len = 0;
   DATA_PTR(self) = data;
 
   return self;
 }
 
-static mrb_value mrb_julia_eval(mrb_state *mrb, mrb_value self)
+static mrb_value
+mrb_julia_eval(mrb_state *mrb, mrb_value self)
 {
   char *str;
   char *juliaScript;
   char *returnedString;
   int len;
+  jl_value_t *ret;
 
   mrb_julia_data *data = DATA_PTR(self);
   mrb_get_args(mrb, "s", &str, &len);
-  juliaScript = (char*)malloc(sizeof(char)*(len+1));
+  juliaScript = (char *)malloc(sizeof(char) * (len + 1));
   memcpy(juliaScript, str, len);
-  juliaScript[len]='\0';
+  juliaScript[len] = '\0';
 
   /* run julia commands */
-  jl_value_t *ret = jl_eval_string(juliaScript);
-  if(jl_is_byte_string(ret)) {
-    //fprintf(stderr,"jl_is_string == true!\n");
-    returnedString = (char*)jl_bytestring_ptr(ret);
+  ret = jl_eval_string(juliaScript);
+  if (jl_exception_occurred()) {
+    fprintf(stderr, "julia exception occurred!\n");
+    return mrb_nil_value();
+  }
+  if (jl_is_byte_string(ret)) {
+    // fprintf(stderr,"jl_is_string == true!\n");
+    returnedString = (char *)jl_bytestring_ptr(ret);
     data->str = returnedString;
-    //data->len = jl_array_len((jl_array_t*)ret);
-    data->len=strlen(returnedString);
-    //fprintf(stderr,"len = %d\n",data->len);
+    // data->len = jl_array_len((jl_array_t*)ret);
+    data->len = strlen(returnedString);
+    // fprintf(stderr,"len = %d\n",data->len);
     data->str[data->len] = '\0';
   }
-
 
   return mrb_str_new(mrb, data->str, data->len);
 }
 
-void mrb_mruby_julia_gem_init(mrb_state *mrb)
+static mrb_value
+mrb_export_mrb_state(mrb_state *mrb, mrb_value self)
 {
-    struct RClass *julia;
-    julia = mrb_define_class(mrb, "Julia", mrb->object_class);
-    mrb_define_method(mrb, julia, "initialize", mrb_julia_init, MRB_ARGS_NONE());
-    mrb_define_method(mrb, julia, "eval", mrb_julia_eval, MRB_ARGS_REQ(1));
-    //mrb_define_class_method(mrb, julia, "hi", mrb_julia_hi, MRB_ARGS_NONE());
-    jl_init(JULIA_INIT_DIR);
-    DONE;
+  jl_function_t *func;
+  jl_value_t *argument;
+  jl_value_t *ret;
+
+  func = jl_get_function(jl_main_module, "set_mrb_state");
+  argument = jl_box_voidpointer((void *)mrb);
+  ret = jl_call1(func, argument);
+  if (jl_exception_occurred()) {
+    fprintf(stderr, "julia exception occurred!\n");
+    return mrb_nil_value();
+  }
+  return self;
 }
 
-void mrb_mruby_julia_gem_final(mrb_state *mrb)
+void
+mrb_mruby_julia_gem_init(mrb_state *mrb)
 {
-  fprintf(stderr,"mrb_mruby_julia_gem_final: start\n");
-  jl_atexit_hook(0);
-  fprintf(stderr,"mrb_mruby_julia_gem_final: end\n");
+  struct RClass *julia;
+  julia = mrb_define_class(mrb, "Julia", mrb->object_class);
+  mrb_define_method(mrb, julia, "initialize", mrb_julia_init, MRB_ARGS_NONE());
+  mrb_define_method(mrb, julia, "eval", mrb_julia_eval, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, julia, "eval", mrb_julia_eval, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, julia, "export_mrb_state", mrb_export_mrb_state, MRB_ARGS_NONE());
+  // mrb_define_class_method(mrb, julia, "hi", mrb_julia_hi, MRB_ARGS_NONE());
+  jl_init(JULIA_INIT_DIR);
+  DONE;
+}
 
+void
+mrb_mruby_julia_gem_final(mrb_state *mrb)
+{
+  fprintf(stderr, "mrb_mruby_julia_gem_final: start\n");
+  jl_atexit_hook(0);
+  fprintf(stderr, "mrb_mruby_julia_gem_final: end\n");
 }
